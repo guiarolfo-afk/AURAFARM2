b455dc143d72c68083f48808d93878133100385d
guiarolfo <gui.arolfo@gmail.com>
2026-08-29 09:56:39 +0200

    Add missing Tailwind vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/AURAFARM2/',
   root: 'src',
   build: {
     outDir: '../docs',
+    emptyOutDir: true,
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
