87b43f916d2730dc877af51c26c5f9e4fc9f291e
guiarolfo <gui.arolfo@gmail.com>
2026-08-29 09:28:45 +0200

    Debug: add console logs

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+console.log("main.tsx cargado");
+console.log("root element:", document.getElementById("root"));
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
